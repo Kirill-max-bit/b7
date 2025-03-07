959aaeaf59e449136d89f8788964f6daaa2be502
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-07 07:20:06 +0000

    сделал задачу

--- main61.py
+++ main61.py
@@ -1,3 +1,8 @@
 def best_skier(times):
     min_time = min(times)
     return times.index(min_time) + 1
+
+
+times = [120, 110, 115, 110, 130]
+skier = best_skier(times)
+print(f"Лыжник с лучшим результатом стартовал под номером: {skier}")
