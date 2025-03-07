e8c7a6998f3b02edabc8c2faed66ac62ebd74b7d
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-07 07:46:26 +0000

    сделал задачу

--- main74.py
+++ main74.py
@@ -6,3 +6,13 @@ def count_max_elements(sequence):
 sequence = [3, 5, 8, 8, 4]
 result = count_max_elements(sequence)
 print(f"Количество максимальных элементов: {result}")
+
+
+def count_min_elements(sequence):
+    min_element = min(sequence)
+    return sequence.count(min_element)
+
+
+sequence = [3, 5, 1, 1, 4]
+result = count_min_elements(sequence)
+print(f"Количество минимальных элементов: {result}")
