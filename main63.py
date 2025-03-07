920b2554e786becfb23fc3e09d837a8f007fe9f1
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-07 07:21:24 +0000

    сделал задачу

--- main63.py
+++ main63.py
@@ -2,3 +2,8 @@ def max_precipitation_day(precipitations):
     max_precip = max(precipitations)
     last_day = len(precipitations) - 1 - precipitations[::-1].index(max_precip)
     return last_day + 1
+
+
+precipitations = [5, 10, 15, 10, 20, 15]
+day = max_precipitation_day(precipitations)
+print(f"Самое большое количество осадков выпало {day} числа")
