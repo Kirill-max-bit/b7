8e618c788cdc7210e6ba2bc51046572fca816741
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-07 07:23:38 +0000

    сделал задачу

--- main66.py
+++ main66.py
@@ -1,2 +1,8 @@
 def class_size_diff(classes):
     return max(classes) - min(classes)
+
+
+classes = [25, 30, 20, 28, 22]
+diff = class_size_diff(classes)
+print(f"Разница в численности классов: {diff} учеников")
+
