5a569f7cd37eccf25413eb46001eda66d20fa54c
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-03-07 07:48:57 +0000

    сделал задачу

--- main72.py
+++ main72.py
@@ -1,10 +1,9 @@
 def min_current_resistance(voltages, resistances):
-    currents = [voltage / resistance for voltage, resistance in zip(voltages, resistances)]
-    min_current = min(currents)
-    return currents.index(min_current) + 1
+    currents = [v / r for v, r in zip(voltages, resistances) if r != 0]
+    return currents.index(min(currents)) + 1
 
 
-voltages = [10, 20, 15]  # вольты
-resistances = [5, 10, 3] # омы
+voltages = [10, 20, 15]
+resistances = [5, 10, 3]
 result = min_current_resistance(voltages, resistances)
 print(f"Сопротивление с минимальным током: №{result}")
